873103b3d5ca594cf7ccb3d1b4f5a2dd09a799ad
Glory_Kuria <kuriaglory@gmail.com>
2024-03-29 19:40:05 +0300

    add files

---
+++ functions.js
@@ -0,0 +1,30 @@
+function sort(){
+    let words=["home","country","place"];
+    console.log(words.sort());
+    console.log(words.reverse());
+}
+sort()
+
+//  var array=[10,4,-4,-2];
+//  array.forEach (i=>{
+//     if(i<0)
+//      neg-count++;
+//  else
+//  post-count ++;
+// });
+// console.log(`neg-count`)
+// console.log(`post-count`);
+
+
+// const employee=[{id:a}, {name:"Amalia"},{salary:[30000,20000]}]
+//  employee.sort(function(a,b){
+//      return a.salary-b.salary;
+//  })
+//  console.log(employee);
+
+
+
+ const number=[3,4,5,6];
+ number.forEach((y)=>{
+     console.log(y*2);
+ })
