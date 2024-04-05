e64f9a8f51f7c5272d0e112dde61367dc8782729
Glory_Kuria <kuriaglory@gmail.com>
2024-04-05 17:15:07 +0300

    add files

---
+++ control.js
@@ -0,0 +1,84 @@
+// Write a program that takes in an array of numbers and consoles the first four items multiplied by itself 
+//  and the last two added by 10. Return the array with the new values
+// function arrayOfNumbers(items){
+//     let newarray =[];
+//    for (let i =0; i <items.length; i++){
+//         if(i<=3){
+//       let result = items[i]*items[i];{
+//       newarray.push(result);
+//       return newarray;
+//    } }
+//    else(i<=-2);{
+//     let result = items[i] +10;
+//     newarray.push(result)
+//    }
+// }
+// }
+
+//  console.log(arrayOfNumbers([10,11,2,3,4,5,67,8,10,9,15,13]))
+
+// console.log(multiply([2,3,4,6,7]));
+
+ function arrayOfNumbers(items){
+    let num = items.slice(0,4)
+    let newnums = items.slice(-2) 
+    num.forEach(i=>{
+        console.log(i*i)
+    })
+    newnums.forEach(i=>{
+        console.log(i+10)
+    })
+ }
+ arrayOfNumbers([10,11,2,3,4,5,67,8,10,9,15,13])
+
+
+
+// Write a program that takes in the following array and use a while loop to iterate and break when
+//  the item is equal to the fourth index :let arrNum = [1,2,3,4,5,6,7,8,9];
+// let arrNum = [1,2,3,4,5,6,7,8,9] 
+// function takeArray(num) {
+//  while(num === arrNum[4]){
+// break;
+//     }
+// }
+
+
+// Write a function that takes in a an array of strings and use a continue statement when 
+//  item is at the second index:  let fruits= ['apple','plum','banana','strawberries','kiwi']
+
+function arrayStrings(item){
+    for(let i = 0; i < item.length; i++){
+        if  (i ===2){
+            continue;
+        }
+        console.log(item[i]);
+    }
+}
+arrayStrings(['apple','plum','banana','strawberries','kiwi'])
+
+
+// Write a function that accepts an array of strings and console.logs each element using a for loop.
+function acceptArray(strings){
+    for(let i =0; i<strings.length;i++){
+        console.log(strings[i])
+    }
+}
+acceptArray(["Glory","jane","ken","loice"])
+
+
+// Write a JavaScript function that takes a string as input and reverses it using a while loop.
+//  The function should return the reversed string.
+function takeString(){
+    let name = "yvonne"
+while ('yvonne'){
+console.log(name.sort(""))
+}
+}
+takeString()
+
+// const leg = true
+// const kikckBall = (leg) => {
+//     while (leg){
+//         console.log('kick the ball');
+//         break;
+//     }
--- functions.js
+++ functions.js
@@ -1,32 +1,61 @@
 // use a function to reverse all the elements in the string in ascending order and the specific ements in descending order
 
 function sort(){
-    let words=["home","country","place"];
-    console.log(words.sort());
-    console.log(words.reverse());
+    newarray =[]
+    let words=["home","country","place","palestine","parking","pole"];
+    words.forEach(i => {
+        if(i[0]=="p"||i[0]=="P"){
+        let newwords=i.split("").sort().reverse("").join("")
+        newarray.push(newwords)
+        console.log(newarray);
+        }
+        // else{
+        //     let newwords = i.split("").sort().join("")
+        //     newarray.push(newwords)
+        // }
+    }
+        )
+        console.log(newarray)
+    
 }
 sort()
 
 // return positive if an element within the array is positive,negative if an element is negative,else zero
-function num(number){
+function num(){
+    let element=[]
+    let number =  [2,-3,4,0,-5]
     number.forEach(i=>{
         if(i>0){
-            console.log('positive');
+            let result = "positive"
+            element.push(result)
         }
-        else{
-            console.log('negative')
-        }
-        if(i==0){
-            console.log('zero')
+        else if(i<0){
+            result ="negative"
+            element.push(result)
+        
+    }
+       else{
+            result = "zero"
+            element.push(result)
         }    
     });
+    
+    return element
 }
-    num([5,-2,-1,7,0]);
+
+// let a = [5,-2,-1,7,0]
+   console.log( num());
 
     
-// function employees(){
-
-// }
+ function employees(){
+let arremployees = [
+    {name:"Rachael",id:"g5766f",salary:100000},
+    {name:"Glory",id:"3546790",salary:100000},
+    {name:"Jane",id:"46fhfg7",salary:30000}
+]
+return (arremployees.sort ((b,a) => a.salary -b.salary))
+ }
+ console.log (employees())
 
 // write a function that takes in an array of numbers and uses the forEach() method to console.log each number multiplied by2
 function numbers(nums){
@@ -38,15 +67,43 @@ numbers([12,16,70,80,50,])
 
 // write a function that takes in an array ofnumbers and consoles the first four items multiplied by 8
 // and the last two added by 5
-function array(items){
-    let newvalue=items.slice(0,4)
+ function array(items){
+     let newvalue=items.slice(0,4)
     let newvalue2=items.slice(-2)
-    newvalue.forEach(i=>{
+   newvalue.forEach(i=>{
         console.log(i*8);
-    })
-    newvalue2.forEach(i=>{
+   })
+     newvalue2.forEach(i=>{
         console.log(i +5)
     })
-}
+ }
 array([10,12,4,90,5,73,20,5,8])
+
+
+function array(numbers){
+    let num = numbers.slice(0,4)
+    let newnums = numbers.slice(-4) 
+    num.forEach(i=>{
+        console.log(i*i)
+    })
+    newnums.forEach(i=>{
+        console.log()
+    })
+}
+array([11,12,50,3,6,6,7,0,5])
+
+// const numbers =[4,6,8,10,12,11]
+// function array(numbers){
+//     numbers.forEach((number,index)=>{
+// if (index<4){
+//     numbers[index]=number*8
+// }
+//     else if(index>=numbers.length-2){
+//         numbers [index]= number+5
+//     }
+//     })
+
+// console.log(numbers)
+// }
+// array(numbers)
 
