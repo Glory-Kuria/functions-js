ac7b032f81b704dc467fe09a20d8ece6837bf761
Glory_Kuria <kuriaglory@gmail.com>
2024-04-07 20:14:10 +0300

    add changes

--- control.js
+++ control.js
@@ -2,7 +2,7 @@
 //  and the last two added by 10. Return the array with the new values
 
  function arrayOfNumbers(items){
-    let num = items.slice(0,4)
+let num = items.slice(0,4)
     let newnums = items.slice(-2) 
     num.forEach(i=>{
         console.log(i*i)
@@ -10,55 +10,69 @@
     newnums.forEach(i=>{
         console.log(i+10)
     })
+  }
+  arrayOfNumbers([10,11,2,3,4,5,67,8,10,9,15,13])
+
+
+
+// // Write a program that takes in the following array and use a while loop to iterate and break when
+// //  the item is equal to the fourth index :let arrNum = [1,2,3,4,5,6,7,8,9];
+ function takeArray() {
+    let array = [1,2,3,4,5,6,7,8,9]
+    const arr =array.slice(0,5)
+  while(arr){
+    console.log(arr)
+  break;
+     }
  }
- arrayOfNumbers([10,11,2,3,4,5,67,8,10,9,15,13])
+ takeArray()
 
 
-
-// Write a program that takes in the following array and use a while loop to iterate and break when
-//  the item is equal to the fourth index :let arrNum = [1,2,3,4,5,6,7,8,9];
-// let arrNum = [1,2,3,4,5,6,7,8,9] 
-//  function takeArray() {
-//     let array = [1,2,3,4,5,6,7,8,9]
-//     const arr =num.slice(0,5)
-//  while(arr){
-//     console.log(arr)
-//  break;
-//     }
-// }
-// takeArray()
-
-
-// Write a function that takes in a an array of strings and use a continue statement when 
-//  item is at the second index:  let fruits= ['apple','plum','banana','strawberries','kiwi']
-
- function arrayStrings(item){
+// // Write a function that takes in a an array of strings and use a continue statement when 
+// //  item is at the second index:  let fruits= ['apple','plum','banana','strawberries','kiwi']
+  function arrayStrings(item){
      for(let i = 0; i < item.length; i++){
-        if  (i ===2){
-            continue;
+         if  (i ===2){
+          continue;
          }
          console.log(item[i]);
     }
-}
- arrayStrings(['apple','plum','banana','strawberries','kiwi'])
+ }
+  arrayStrings(['apple','plum','banana','strawberries','kiwi'])
 
 
-// Write a function that accepts an array of strings and console.logs each element using a for loop.
- function acceptArray(strings){
+// // Write a function that accepts an array of strings and console.logs each element using a for loop.
+  function acceptArray(strings){
      for(let i =0; i<strings.length;i++){
        console.log(strings[i])
     }
-}
-acceptArray(["Glory","jane","ken","loice"])
+ }
+ acceptArray(["Glory","jane","ken","loice"])
 
 
 // Write a JavaScript function that takes a string as input and reverses it using a while loop.
 //   The function should return the reversed string.
-//  function takeString(){
-//      let name = "yvonne"
-// while ('yvonne'){
-// console.log(name.sort(""))
-// }
-// }
-// takeString()
+ function takeString(name){ 
+ while ('name'){
+ console.log(name.split("").reverse("").join(""))
+ break;
+}
+return name.split("").reverse("").join("")
+
+ }
+takeString('yvonne')
+
+function takesNum(numbers){
+    let nums = numbers.slice(0,4)
+    let newnum = numbers.slice(-2)
+    nums.forEach(i =>{
+console.log(i*3)
+})
+newnum.forEach( i=>{
+    console.log(i+10)
+})
+    return numbers
+}
+takesNum([1,2,3,4,5]);
+
 
